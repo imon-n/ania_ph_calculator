f1f6468b613c1fa920413ed701ee15046190a38f
imon <imon.eee@gmail.com>
2025-03-20 14:50:57 +0600

    issue solved

--- assignmet_ph_calcu/settings.py
+++ assignmet_ph_calcu/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ['https://ania-ph-calculator.onrender.com/','https://*.127.0.0.1']
+# CSRF_TRUSTED_ORIGINS = ['https://ania-ph-calculator.onrender.com/','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ["https://ania-ph-calculator.onrender.com"]
 
 
 # Application definition
